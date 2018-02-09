fa3bc242c251520428349eabe2484d029cf08218
John Furtado <jf8846@gmail.com>
2018-02-08 19:19:28 -0500

    Abstract classes

--- src/com/Madpoints/Main.java
+++ src/com/Madpoints/Main.java
@@ -3,6 +3,18 @@ package com.Madpoints;
 public class Main {
 
     public static void main(String[] args) {
-       
+        Dog dog = new Dog("Corgie");
+        dog.breathe();
+        dog.eat();
+
+        Parrot parrot = new Parrot("Parrot");
+        parrot.eat();
+        parrot.breathe();
+        parrot.fly();
+
+        Penguin penguin = new Penguin("Emperor");
+        penguin.eat();
+        penguin.breathe();
+        penguin.fly();
     }
 }
