b4044ac8ff62aeed4b9d3bc77f8b3faf65f2c4dc
John Furtado <jf8846@gmail.com>
2018-02-27 18:49:10 -0500

    generics exercise

--- src/com/Madpoints/Main.java
+++ src/com/Madpoints/Main.java
@@ -3,19 +3,25 @@ package com.Madpoints;
 public class Main {
 
     public static void main(String[] args) {
-        FootballPlayer teddyBruschi = new FootballPlayer("Teddy Bruschi");
+        League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
         Team<FootballPlayer> patriots = new Team<>("New England Patriots");
-        patriots.addPlayer(teddyBruschi);
-
+        Team<FootballPlayer> jets = new Team<>("New York Jets");
+        Team<FootballPlayer> bills = new Team<>("Buffalo Bills");
+        Team<FootballPlayer> dolphins = new Team<>("Miami Dolphins");
         Team<BaseballPlayer> redSox = new Team<>("Boston Red Sox");
-        BaseballPlayer davidOrtiz = new BaseballPlayer("David Ortiz");
-        redSox.addPlayer(davidOrtiz);
-        // cannot add to baseball team, not correct type
-        //redSox.addPlayer(teddyBruschi);
 
-        Team<SoccerPlayer> revolution = new Team<>("New England Revolution");
-        SoccerPlayer wayneRooney = new SoccerPlayer("Wayne Rooney");
-        revolution.addPlayer(wayneRooney);
+        //patriots.gameResult(jets, 21, 14);
+//        patriots.gameResult(bills, 23, 17);
+//        jets.gameResult(dolphins, 24, 12);
 
+        footballLeague.add(patriots);
+        footballLeague.add(jets);
+        footballLeague.add(bills);
+        footballLeague.add(dolphins);
+        // cannot add baseball team to football league
+        // incompatible types
+        //footballLeague.add(redSox);
+
+        footballLeague.showLeagueTable();
     }
 }
